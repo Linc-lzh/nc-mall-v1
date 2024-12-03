6b38c98145789a8a54aba1ff321a1561799b28c7
linc-lzh <linc_li@sina.cn>
2024-12-04 00:48:57 +0800

    Check in init code

---
+++ src/main/java/nc/mall/modules/ums/mapper/UmsAdminRoleRelationMapper.java
@@ -0,0 +1,21 @@
+package nc.mall.modules.ums.mapper;
+
+import nc.mall.modules.ums.model.UmsAdminRoleRelation;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import nc.mall.modules.ums.model.UmsResource;
+import nc.mall.modules.ums.model.UmsRole;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 后台用户和角色关系表 Mapper 接口
+ * </p>
+ *
+ * @author macro
+ * @since 2020-08-21
+ */
+public interface UmsAdminRoleRelationMapper extends BaseMapper<UmsAdminRoleRelation> {
+
+}
